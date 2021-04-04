4450ebb7d756cf6b45a64f00bf898c4ade53de07
patrycjakubiczek <p.kubiczek@gmail.com>
2021-04-04 10:51:19 +0200

    update to node-sass

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-import "./styles/main.css";
+import "./styles/main.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
