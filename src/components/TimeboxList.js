d2d4429e15a91dcb6aba7f093330a8bd19f505ea
patrycjakubiczek <p.kubiczek@gmail.com>
2021-04-21 12:55:35 +0200

    add axios

--- src/components/TimeboxList.js
+++ src/components/TimeboxList.js
@@ -4,24 +4,20 @@ import { Paper } from "@material-ui/core";
 import Timebox from "./Timebox";
 import TimeboxCreator from "./TimeboxCreator";
 import ErrorBoundary from "./ErrorBoundary";
-import TimeboxesAPI from "../api/FetchTimeboxesAPI";
+import TimeboxesAPI from "../api/AxiosTimeboxesAPI";
 
 class TimeboxList extends React.Component {
   state = {
-    "timeboxes": [],
+    timeboxes: [],
     loading: true,
     error: null,
   };
 
   componentDidMount() {
     TimeboxesAPI.getAllTimeboxes()
-      .then(
-        (timeboxes) => this.setState({ timeboxes })
-      ).catch(
-        (error) => this.setState({ error })
-      ).finally(
-        () => this.setState({ loading: false })
-      );
+      .then((timeboxes) => this.setState({ timeboxes }))
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
   }
 
   addTimebox = (timebox) => {
@@ -33,40 +29,24 @@ class TimeboxList extends React.Component {
     );
   };
   removeTimebox = (indexToRemove) => {
-    TimeboxesAPI.removeTimebox(this.state.timeboxes[indexToRemove])
-    .then(
-      () => {
+    TimeboxesAPI.removeTimebox(this.state.timeboxes[indexToRemove]).then(() => {
       this.setState((prevState) => {
         const timeboxes = prevState.timeboxes.filter(
           (timebox, index) => index !== indexToRemove
         );
         return { timeboxes };
+      });
     });
-  });
-  }
-  updateTimebox = (indexToUpdate, timeboxToUpdate) => {
-    TimeboxesAPI.replaceTimebox(timeboxToUpdate)
-    .then((updatedTimebox) => {
+  };
+  updateTimebox = (indexToUpdate, updatedTimebox) => {
+    console.log(updatedTimebox);
+    TimeboxesAPI.replaceTimebox(updatedTimebox).then((updatedTimebox) => {
       this.setState((prevState) => {
         const timeboxes = prevState.timeboxes.map((timebox, index) =>
           index === indexToUpdate ? updatedTimebox : timebox
         );
         return { timeboxes };
       });
-    });
-
-    
-  };
-  handleCreate = (createdTimebox) => {
-    this.addTimebox(createdTimebox);
-  };
-  handleSave = (indexToUpdate, updatedTimebox) => {
-    console.log(updatedTimebox);
-    this.setState((prevState) => {
-      const timeboxes = prevState.timeboxes.map((timebox, index) =>
-        index === indexToUpdate ? updatedTimebox : timebox
-      );
-      return { timeboxes };
     });
   };
   handleEdit = (indexToUpdate) => {
@@ -80,12 +60,12 @@ class TimeboxList extends React.Component {
   render() {
     return (
       <>
-        <TimeboxCreator onCreate={this.handleCreate} />
+        <TimeboxCreator onCreate={this.addTimebox} />
         {this.state.loading && <p>timeboxy się ładują</p>}
 
         <h2>Obecna lista zadań:</h2>
         {this.state.error ? this.state.error : null}
-        <ErrorBoundary message={ "Błąd"}>
+        <ErrorBoundary message={"Błąd"}>
           {this.state.timeboxes.map((timebox, index) => (
             <Timebox
               key={timebox.id}
@@ -95,7 +75,7 @@ class TimeboxList extends React.Component {
               totalTimeInMinutes={timebox.totalTimeInMinutes}
               onDelete={() => this.removeTimebox(index)}
               onEdit={() => this.handleEdit(index)}
-              onSave={this.handleSave}
+              onSave={this.updateTimebox}
               isEdited={timebox.isEdited}
             />
           ))}
