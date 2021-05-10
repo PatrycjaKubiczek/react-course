deba138bbf1817bb0c4d7be6228de1acaeab365a
patrycjakubiczek <p.kubiczek@gmail.com>
2021-05-10 21:16:31 +0200

    change timeboxcreator to functional component

--- src/components/TimeboxCreator.js
+++ src/components/TimeboxCreator.js
@@ -1,47 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, TextField, Paper } from "@material-ui/core";
 
-class TimeboxCreator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.titleInput = React.createRef();
-    this.totalTimeInMinutesInput = React.createRef();
-  }
-  handleSubmit = (e) => {
-    console.log(this.titleInput.current.value);
+function TimeboxCreator({ onCreate }) {
+  const titleInput = useRef();
+  const totalTimeInMinutesInput = useRef();
+
+  function handleSubmit(e) {
     e.preventDefault();
-    this.props.onCreate({
-      title: this.titleInput.current.value,
-      totalTimeInMinutes: this.totalTimeInMinutesInput.current.value,
+    onCreate({
+      title: titleInput.current.value,
+      totalTimeInMinutes: totalTimeInMinutesInput.current.value,
       isEdited: false,
     });
-    this.titleInput.current.value = "";
-     this.totalTimeInMinutesInput.current.value = "";
-  };
-  render() {
-    return (
-      <>
-        <h2>Dodaj nowe zadanie</h2>
-        <form className="CurrentTimebox"> 
-         <TextField
-          inputRef={this.titleInput}
+    titleInput.current.value = "";
+    totalTimeInMinutesInput.current.value = "";
+  }
+  return (
+    <>
+      <h2>Dodaj nowe zadanie</h2>
+      <form className="CurrentTimebox">
+        <TextField
+          inputRef={titleInput}
           label="Co robisz?"
           variant="outlined"
         />
         <TextField
-          inputRef={this.totalTimeInMinutesInput}
+          inputRef={totalTimeInMinutesInput}
           label="Ile minut?"
           type="number"
           variant="outlined"
         />
-      <Button variant="outlined" onClick={this.handleSubmit}>
+        <Button variant="outlined" onClick={handleSubmit}>
           dodaj timebox
         </Button>
-     </form>
-       
-      </>
-    );
-  }
+      </form>
+    </>
+  );
 }
 
 export default TimeboxCreator;
