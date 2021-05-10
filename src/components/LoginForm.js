f1f20176c2d304a0fedc046751f252eb8946cf20
patrycjakubiczek <p.kubiczek@gmail.com>
2021-05-10 21:03:48 +0200

    changed loginForm component from class to functional components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,48 +1,45 @@
-import React from "react";
-import { Button, TextField, Paper } from "@material-ui/core";
+import React, { useRef } from "react";
+import { Button, TextField } from "@material-ui/core";
 
-class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.emailInput = React.createRef();
-    this.passwordInput = React.createRef();
-  }
-  handleSubmit = (e) => {
-    console.log(this.emailInput.current.value);
+function LoginForm({ errorMsg, onLoginAttempt }) {
+  const emailInput = useRef();
+  const passwordInput = React.createRef();
+
+  //THIS FUNCTION BELOW NEED TO BE CHANGED
+  function handleSubmit(e) {
     e.preventDefault();
-    this.props.onLoginAttempt({
-      email: this.emailInput.current.value,
-      password: this.passwordInput.current.value,
+    onLoginAttempt({
+      email: emailInput.current.value,
+      password: passwordInput.current.value,
     });
-    this.emailInput.current.value = "";
-    this.passwordInput.current.value = "";
-  };
-  render() {
-    return (
-      <>
-        <h2>Dodaj nowe zadanie</h2>
-        <div>{this.props.errorMsg}</div>
-        <form className="CurrentTimebox">
-          <TextField
-            inputRef={this.emailInput}
-            label="email"
-            variant="outlined"
-            defaultValue="grazynka@test.pl"
-          />
-          <TextField
-            inputRef={this.passwordInput}
-            label="hasło"
-            type="password"
-            variant="outlined"
-            defaultValue="haslo"
-          />
-          <Button variant="outlined" onClick={this.handleSubmit}>
-            zaloguj się
-          </Button>
-        </form>
-      </>
-    );
+    emailInput.current.value = "";
+    passwordInput.current.value = "";
   }
+
+  return (
+    <>
+      <h2>Dodaj nowe zadanie</h2>
+      <div>{errorMsg}</div>
+      <form className="CurrentTimebox">
+        <TextField
+          inputRef={emailInput}
+          label="email"
+          variant="outlined"
+          defaultValue="grazynka@test.pl"
+        />
+        <TextField
+          inputRef={passwordInput}
+          label="hasło"
+          type="password"
+          variant="outlined"
+          defaultValue="haslo"
+        />
+        <Button variant="outlined" onClick={handleSubmit}>
+          zaloguj się
+        </Button>
+      </form>
+    </>
+  );
 }
 
 export default LoginForm;
